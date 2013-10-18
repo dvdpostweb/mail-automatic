074236c376cd1ca63c6f73b6f8e7c91a2411ed91
guillaume <gs@dvdpost.be>
2013-10-18 14:29:52 +0200

    propose2

--- script/propose_vod2.php
+++ script/propose_vod2.php
@@ -11,7 +11,7 @@ class Propose_vod extends Script {
 		join products p on w.product_id = p.products_id
 		left join vod_wishlists v on v.customer_id = c.customers_id and v.imdb_id = p.imdb_id
 		join propose_vods pv on p.imdb_id = pv.imdb_id
-		where customers_abo=1 and customers_abo_suspended = 0 and entry_country_id=21  and v.imdb_id is null and date(w.`date_added`)=date(date_add(now(),interval -7 DAY)) and (select count(*) from streaming_products where imdb_id = p.imdb_id and
+		where customers_abo=1 and customers_abo_suspended = 0 and entry_country_id=21  and v.imdb_id is null and date(w.`date_added`)<=date(date_add(now(),interval -7 DAY)) and (select count(*) from streaming_products where imdb_id = p.imdb_id and
 		streaming_products.status = "online_test_ok" and (streaming_products.available_from <= date(date_add(now(),interval -10 day)) and streaming_products.expire_at >= date(date_add(now(),interval -10 day)) or (streaming_products.available_backcatalogue_from <= date(now()) and streaming_products.expire_backcatalogue_at >= date(now()))) and available = 1) and mail_id = '.$mail_id.' and (customers_language = if(fr = 1,1,0) or customers_language = if(nl = 1,2,0) or customers_language = if(en = 1,3,0));';
 		$this->data = tep_db_query($sql_data);
 	}
