c3abc6ad16b888a5101da7af4dbcedb836fd1337
guillaume <gs@dvdpost.be>
2014-02-24 16:45:28 +0100

    debug

--- script_plush/launch_48.php
+++ script_plush/launch_48.php
@@ -8,7 +8,7 @@ class launch_48 extends Script {
 	}
 	public function execute($mail_id)
 	{
-		$sql_data='select email customers_email, customers_language from discount_use d
+		$sql_data='select email customers_email, customers_language, c.customers_id from discount_use d
     join abo a on a.customerid = d.customers_id
     join customers c on d.customers_id = c.customers_id
     where action=4 and `discount_code_id`in (105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120) and customers_abo!=1
--- script_plush/launch_48_fb.php
+++ script_plush/launch_48_fb.php
@@ -8,7 +8,7 @@ class launch_48_fb extends Script {
 	}
 	public function execute($mail_id)
 	{
-		$sql_data='select email customers_email, customers_language from discount_use d
+		$sql_data='select email customers_email, customers_language, c.customers_id from discount_use d
     join abo a on a.customerid = d.customers_id
     join customers c on d.customers_id = c.customers_id
     where action=4 and `discount_code_id`in (121) and customers_abo!=1
