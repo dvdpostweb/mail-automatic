1a91d5e5b4932b1d036a10c01e74867d8457a38d
guillaume <gs@dvdpost.be>
2014-02-24 16:39:41 +0100

    630

--- script_plush/launch_48_fb.php
+++ script_plush/launch_48_fb.php
@@ -11,7 +11,7 @@ class launch_48_fb extends Script {
 		$sql_data='select email customers_email, customers_language from discount_use d
     join abo a on a.customerid = d.customers_id
     join customers c on d.customers_id = c.customers_id
-    where action=4 and `discount_code_id`in (121) and date(discount_use_date) = date(now())
+    where action=4 and `discount_code_id`in (121) and customers_abo!=1
     group by c.customers_id;';
 		$this->data = tep_db_query($sql_data);
 	}
