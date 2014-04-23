17d9e8eaea1907b85a4faab0b33de485274afd89
guillaume <gs@dvdpost.be>
2014-04-23 17:22:11 +0200

    debug

--- script_plush/mobistar_reply.php
+++ script_plush/mobistar_reply.php
@@ -7,7 +7,7 @@ class mobistar_reply extends Script {
 	}
 	public function execute($mail_id)
 	{
-		$sql_data='select 0 customers_id, 1 customers_language, 1376451 imdb_id, name streaming_code, email, email customers_email, streaming_codes.* from `streaming_codes` where email is not null and used_at is null and date(updated_at) <= date(DATE_ADD(now(), INTERVAL -3 day))';
+		$sql_data='select 0 customers_id, 1 customers_language, 1376451 imdb_id, name streaming_code, email, email customers_email, streaming_codes.* from `streaming_codes` where email is not null and used_at is null and date(updated_at) = date(DATE_ADD(now(), INTERVAL -3 day))';
 		$this->data = tep_db_query($sql_data);
 	}
 	function add_data_row($data)
