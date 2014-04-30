5fcd02862c99265ba1debfaf5269ee6041f047fa
guillaume <gs@dvdpost.be>
2014-04-30 17:04:40 +0200

    add mobistar retry

--- script_plush/mobistar_after_video.php
+++ script_plush/mobistar_after_video.php
@@ -7,7 +7,7 @@ class mobistar_after_video extends Script {
 	}
 	public function execute($mail_id)
 	{
-		$sql_data='select 0 customers_id, 1 customers_language,s.*, email customers_email from streaming_codes s
+		$sql_data='select 0 customers_id, 1 customers_language,s.*, s.email customers_email from streaming_codes s
         left join customers c on c.email = s.email
         where s.email is not NULL and used_at is not null and c.email is null and date(used_at ) < date(DATE_ADD(NOW(), INTERVAL -3 day))';
 		$this->data = tep_db_query($sql_data);
