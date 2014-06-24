eb27c3cf1f826ee01cc4101db76d8d04ef76aa67
popac <aleksandar.popovic@codepop.biz>
2014-06-24 16:59:05 +0200

    new mail system

--- script_plush/mobistar_after_video.php
+++ script_plush/mobistar_after_video.php
@@ -9,7 +9,7 @@ class mobistar_after_video extends Script {
 	{
 		$sql_data='select 0 customers_id, 1 customers_language,s.*, s.email customers_email from streaming_codes s
         left join customers c on c.email = s.email
-        where s.email is not NULL and used_at is not null and customers_abo !=1 and date(used_at ) = date(DATE_ADD(NOW(), INTERVAL -3 day)) group by s.email';
+        where s.email is not NULL and used_at is not null and customers_abo !=1 and date(used_at ) = date(DATE_ADD(NOW(), INTERVAL -3 day)) and name like "MB%"' group by s.email';
 		$this->data = tep_db_query($sql_data);
 	}
 	/*function add_data_row($data)
