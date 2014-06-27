85ba5adeee002115772488d7da04a2847f1d8f7f
popac <aleksandar.popovic@codepop.biz>
2014-06-27 14:36:18 +0200

    change language for exp_reply

--- script_plush/exp_reply.php
+++ script_plush/exp_reply.php
@@ -7,7 +7,9 @@ class exp_reply extends Script {
 	}
 	public function execute($mail_id)
 	{
-		$sql_data='select 0 customers_id, 1 customers_language, 1645155 imdb_id, name streaming_code, email, email customers_email, streaming_codes.* from `streaming_codes` where email is not null and used_at is null and date(updated_at) = date(DATE_ADD(now(), INTERVAL -3 day)) and (name like "EXP%" or name like "TEXP%" or name like "SEXP%" or name like "FEXP%" or name like "WEXP%" ) ;';
+		$sql_data='select 0 customers_id, locale_id customers_language, 1645155 imdb_id, name streaming_code, s.email, s.email customers_email, s.* from `streaming_codes` s
+ 		join prospects p on s.email = p.email
+		where s.email is not null and used_at is null and date(s.updated_at) = date(DATE_ADD(now(), INTERVAL -3 day)) and (name like "EXP%" or name like "TEXP%" or name like "SEXP%" or name like "FEXP%" or name like "WEXP%" ) ;';
 		$this->data = tep_db_query($sql_data);
 	}
 	function add_data_row($data)
